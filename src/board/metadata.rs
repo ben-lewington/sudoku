0c35b89d01caf34c854ac9315f98487613d6aa9e
ben <ben.lewington91@ntlworld.com>
2022-08-14 00:59:20 +0100

    all

---
+++ src/board/metadata.rs
@@ -0,0 +1,48 @@
+use crate::utils::array::{new, new_checked};
+use anyhow::Result;
+
+use super::{Board, Segment};
+
+#[derive(Debug)]
+pub(crate) struct Metadata<const N: usize>
+where
+    [(); 3 * N * N]: Sized,
+    [(); N + 1]: Sized,
+{
+    pub(crate) cnts: [usize; 3 * N * N],
+    pub(crate) freqs: [[usize; N + 1]; 3 * N * N],
+}
+
+impl<const N: usize> Metadata<N>
+where
+    [(); N * N * N * N]: Sized,
+    [(); N * N * N * N - 1]: Sized,
+    [(); 3 * N * N]: Sized,
+    [(); N * N - 1]: Sized,
+    [(); N + 1]: Sized,
+{
+    pub fn new(b: &Board<N>) -> Result<Self> {
+        let v = Self {
+            cnts: new((0..N * N)
+                .map(|i| b.segment(Segment::row(i)).sum::<usize>())
+                .chain((0..N * N).map(|i| b.segment(Segment::col(i)).sum::<usize>()))
+                .chain((0..N * N).map(|i| b.segment(Segment::minor(i)).sum::<usize>())))?,
+            freqs: new((0..N * N)
+                .map(|i| {
+                    new((0..=N).map(|v| b.segment(Segment::row(i)).filter(|&w| w == v).count()))
+                        .unwrap()
+                })
+                .chain((0..N * N).map(|i| {
+                    new((0..=N).map(|v| b.segment(Segment::col(i)).filter(|&w| w == v).count()))
+                        .unwrap()
+                }))
+                .chain((0..N * N).map(|i| {
+                    new((0..=N).map(|v| b.segment(Segment::minor(i)).filter(|&w| w == v).count()))
+                        .unwrap()
+                })))?,
+        };
+        Ok(v)
+    }
+
+    // fn
+}
---
+++ src/utils/array.rs
@@ -0,0 +1,112 @@
+use anyhow::{anyhow, Result};
+use std::fmt::Debug;
+
+pub(crate) fn new_checked<const N: usize, T: Copy + Debug + Default>(
+    it: impl Iterator<Item = T>,
+) -> [T; N] {
+    let b = [T::default(); N];
+    fill_mut(b, it)
+}
+
+fn fill_mut<const N: usize, T: Debug>(mut hydrate: [T; N], it: impl Iterator<Item = T>) -> [T; N] {
+    hydrate.iter_mut().zip(it).for_each(|(i, v)| *i = v);
+    hydrate
+}
+
+pub(crate) fn new<const N: usize, T>(mut it: impl Iterator<Item = T>) -> Result<[T; N]> {
+    // Branch here depending on whether T needs to be dropped.
+    // If it does, we track the amount of cells we've initialised,
+    // and impl Drop on this, to ensure that our progress is rolled back
+    let mut array: std::mem::MaybeUninit<[T; N]> = std::mem::MaybeUninit::uninit();
+
+    let mut ptr_i = array.as_mut_ptr() as *mut T;
+
+    if std::mem::needs_drop::<T>() {
+        //  Safety: The array length is known at compile time, it suffices to
+        //      track counts and assert the pointer arithmetic stays between
+        //      0 and N - 1
+        unsafe {
+            // Count as we go along;
+            // if the iterator stops before N or finishes after we'll return an Err.
+            let mut c = 0;
+            loop {
+                match it.next() {
+                    Some(v) if c <= N - 1 => {
+                        ptr_i.write(v);
+                        ptr_i = ptr_i.add(1);
+                    }
+                    None if c <= N - 1 => {
+                        return Err(anyhow!(
+                            "Specified constructor terminated in {} steps, expected {}",
+                            c,
+                            N
+                        ));
+                    }
+                    Some(_) => {
+                        return Err(anyhow!("Specified constructor is longer than {} steps", N));
+                    }
+                    _ => break,
+                }
+                c += 1;
+            }
+            Ok(array.assume_init())
+        }
+    } else {
+        struct RollbackGuard<T> {
+            base_ptr: *mut T,
+            init_count: usize,
+        }
+
+        impl<T> Drop for RollbackGuard<T> {
+            fn drop(self: &'_ mut Self) {
+                // Safety: The init_count is only incremented after each successful
+                // pointer write.
+                unsafe {
+                    std::ptr::drop_in_place(std::slice::from_raw_parts_mut(
+                        self.base_ptr,
+                        self.init_count,
+                    ));
+                }
+            }
+        }
+
+        let mut panic_guard = RollbackGuard {
+            base_ptr: ptr_i,
+            init_count: 0,
+        };
+
+        unsafe {
+            let mut c = 0;
+            loop {
+                match it.next() {
+                    Some(v) if c <= N - 1 => {
+                        ptr_i.write(v);
+                        ptr_i = ptr_i.add(1);
+                        panic_guard.init_count += 1;
+                    }
+                    None if c <= N - 1 => {
+                        return Err(anyhow!(
+                            "Specified constructor terminated after {} elements , expected {}",
+                            c,
+                            N
+                        ));
+                    }
+                    Some(_) => {
+                        return Err(anyhow!(
+                            "Specified constructor is longer than {} elements",
+                            N
+                        ));
+                    }
+                    _ => {
+                        // leak the panic guard; the array can is initialised
+                        std::mem::forget(panic_guard);
+                        break;
+                    }
+                }
+                c += 1;
+            }
+
+            Ok(array.assume_init())
+        }
+    }
+}
